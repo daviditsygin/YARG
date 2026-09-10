a58706ab2f3685cb87d1725c79520d1464000c11
daviditsygin <daviditsygin@users.noreply.github.com>
2026-09-09 19:44:12 -0700

    Add iOS audio support through BASS and yarg_audio

--- Assets/Editor/Build/IOSBuildPostProcessor.cs
+++ Assets/Editor/Build/IOSBuildPostProcessor.cs
@@ -1,4 +1,5 @@
 #if UNITY_IOS
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -25,6 +26,14 @@ namespace Editor.Build
             project.AddFrameworkToProject(frameworkTarget, "CoreMIDI.framework", false);
 
             project.WriteToFile(projectPath);
+
+            // Vocals capture the microphone through BASS
+            string plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
+            var plist = new PlistDocument();
+            plist.ReadFromFile(plistPath);
+            plist.root.SetString("NSMicrophoneUsageDescription",
+                "YARG uses the microphone for vocals gameplay.");
+            plist.WriteToFile(plistPath);
         }
     }
 }
